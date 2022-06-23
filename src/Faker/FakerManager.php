1a2a60bb2272365b05ffe267e8ea9a5d4ba133a3
araiyusuke <info.yusuke.arai@gmail.com>
2022-06-24 07:45:51 +0900

    不要なコメント削除

--- src/Faker/FakerManager.php
+++ src/Faker/FakerManager.php
@@ -6,6 +6,7 @@ namespace Araiyusuke\FakeApi\Faker;
 
 use Faker\Factory as FakerFactory; 
 use Araiyusuke\FakeApi\Faker\FakerCallBuilder;
+use Araiyusuke\FakeApi\Faker\MatchType;
 
 class MatchResultCollection  {
 
@@ -95,7 +96,6 @@ class FakerMethodMatcher implements Matcher
 
     public function match(string $subject): bool
     {
-
         $matches = [];
         $pattern = $this->type->getPattern();
         preg_match_all("/{$pattern}/", $subject, $matches, PREG_SET_ORDER);
@@ -110,25 +110,6 @@ class FakerMethodMatcher implements Matcher
                 new MatchResultCollectionItem($search, $method, $param ?? null, $id ?? null)
             );
         }
-
-          
-
-
-        // for ($i = 0; $i < $matchCount; $i++) {
-            
-        //     $pattern = $matches[0][$i];
-        //     $param = null;
-            
-        //     if (isset($matches[1][$i])) {
-        //         $param = $matches[1][$i];
-        //     }
-
-        //     $this->result->add(
-        //         new MatchResultCollectionItem($pattern, "method", $param)
-        //     );
-
-        // }
-
         return $this->result->getCount() > 0;
     }
 
@@ -149,36 +130,6 @@ class FakerMethodMatcher implements Matcher
 
     public function setSearch(string $search) {
         $this->search = $search;
-    }
-}
-
-enum MatchType 
-{
-    case methodWithId;
-    case method;
-    case methodParameter;
-    case methodParameterWithId;
-
-    public function getPattern(): string
-    {
-        return match($this) {
-            Self::methodWithId => "%rand_([a-zA-Z0-9]+)_(\d+)%",
-            Self::method => "%rand_([a-zA-Z0-9]+)%",
-            // Self::methodParameter => "%rand_([a-zA-Z0-9]+)\((.*)\)%",
-            Self::methodParameter => "%rand_([a-zA-Z0-9]+)\((.*)\)%",
-
-            Self::methodParameterWithId => "%rand_([a-zA-Z0-9]+)\(([a-zA-Z0-9,]+)\)_(\d+)%"
-        };
-    }
-
-    public function key(): array {
-
-        return match($this) {
-            Self::methodWithId => array('search', 'method', 'id'),
-            Self::method => array('search', 'method'),
-            Self::methodParameter => array('search', 'method', 'param'),
-            Self::methodParameterWithId => array('search', 'method', 'param','id')
-        };
     }
 }
 
@@ -234,123 +185,28 @@ class FakerManager {
 
         foreach($types as $type) {  
 
-                $matcher = new FakerMethodMatcher($type);
+            $matcher = new FakerMethodMatcher($type);
             
-                if ($matcher->match($subject)) {
+            if ($matcher->match($subject)) {
     
-                    foreach($matcher->extractAll()->get() as $match) {
+                foreach($matcher->extractAll()->get() as $match) {
                         
-                        $builder = new FakerCallBuilder;
-                        $builder->setInstance($this->faker);
-                        $builder->setMethod($match->getMethod());
-                        $builder->setArg($match->getParam());
+                    $builder = new FakerCallBuilder;
+                    $builder->setInstance($this->faker);
+                    $builder->setMethod($match->getMethod());
+                    $builder->setArg($match->getParam());
 
-                        $this->replace(
-                            // 置き換える文字列
-                            $match->getSearch(), 
-                            // 置き換える文字列
-                            $builder->call(),
-                            $subject
-                        );
-                    }
-                } 
-            }
-        // }
-
-        // rand_number(33)
-        $matchCount = preg_match_all('/%rand_number\((\d+)\)%/', $subject, $match, PREG_SET_ORDER);
-
-        if ($matchCount !== 0) {
-            for ($i = 0; $i < count($match); $i++) {
-                $subject = str_replace(current($match[$i]), strval($this->faker->randomNumber(($match[$i][1]))), $subject);
-            }
+                      $this->replace(
+                        // 置き換える文字列
+                        $match->getSearch(), 
+                        // 置き換える文字列
+                        $builder->call(),
+                        $subject
+                    );
+                }
+            } 
         }
-
-        // $matchCount = preg_match_all('/%rand_list\[(.+?)\]_(\d+)%/', $subject, $match, PREG_SET_ORDER);
-
-        // if ($matchCount > 0) {
-
-        //     foreach($match as $value) {
-        //         list($x, $words, $id) = $value;
-        //         $list = array_map("trim", explode(",", $words));
-        //         $randValue = $this->faker->randomElement($list);
-        //         $subject = str_replace($x, $randValue, $subject);
-        //     }
-        // }
-
-        $matchCount = preg_match_all('/%rand_number\((\d+)\)_(\d+)%/', $subject, $match, PREG_SET_ORDER);
-
-        if ($matchCount > 0) {
-            foreach($match as $value) {
-                list($x, $digit, $id) = $value;
-                $subject = str_replace($x, strval($this->faker->randomNumber($digit)), $subject);
-            }
-        }
-
-        $matchCount = preg_match_all('/%rand_realText\((\d+)\)%/', $subject, $match, PREG_SET_ORDER);
-
-        if ($matchCount !== 0) {
-            for ($i = 0; $i < count($match); $i++) {
-                $subject = str_replace(current($match[$i]), $this->faker->realText($match[$i][1]), $subject);
-            }
-        }
-
-        $matchCount = preg_match_all('/%rand_list\[(.+?)\]%/', $subject, $match, PREG_SET_ORDER);
-
-        if ($matchCount !== 0) {
-            for ($i = 0; $i < count($match); $i++) {
-                $list = explode(",", $match[$i][1]);
-                $randValue = trim($list[array_rand($list)]);
-                $subject = str_replace(current($match[$i]), $randValue, $subject);
-            }
-        }
-
-        // $matchCount = preg_match_all('/%rand_name_(\d+)%/', $replaced, $match, PREG_SET_ORDER);
-
-        // if ($matchCount !== 0) {
-        //     for ($i = 0; $i < count($match); $i++) {
-        //         $replaced = str_replace(current($match[$i]), $faker->name(), $replaced);
-        //     }
-        // }
-
-        // $matchCount = preg_match_all('/%rand_phoneNumber_(\d+)%/', $replaced, $match, PREG_SET_ORDER);
-
-        // if ($matchCount !== 0) {
-        //     for ($i = 0; $i < count($match); $i++) {
-        //         $replaced = str_replace(current($match[$i]), $faker->phoneNumber(), $replaced);
-        //     }
-        // }
-
-
-        $matchCount = preg_match_all('/%numberBetween\((.+?)\)_(\d+)%/', $subject, $match, PREG_SET_ORDER);
-
-        if ($matchCount > 0) {
-
-
-            foreach($match as $value) {
-
-                list($x, $words, $id) = $value;
-
-                $range = explode(",", $words);
-                
-                $subject = str_replace($x, strval($this->faker->numberBetween($range[0], $range[1])), $subject);
-            }
-        }
-
-        $matchCount = preg_match_all('/%randomFloat\((.+?)\)%/', $subject, $match, PREG_SET_ORDER);
-
-        if ($matchCount > 0) {
-
-
-            foreach($match as $value) {
-
-                list($x, $words) = $value;
-
-                $range = explode(",", $words);
-                
-                $subject = str_replace($x, strval($this->faker->randomFloat($range[0], $range[1], $range[2])), $subject);
-            }
-        }
+    
         return $subject;
     }
 }
---
+++ src/Faker/MatchType.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Araiyusuke\FakeApi\Faker;
+
+enum MatchType 
+
+{
+    case methodWithId;
+    case method;
+    case methodParameter;
+    case methodParameterWithId;
+
+    public function getPattern(): string
+    {
+        return match($this) {
+            Self::methodWithId => "%rand_([a-zA-Z0-9]+)_(\d+)%",
+            Self::method => "%rand_([a-zA-Z0-9]+)%",
+            Self::methodParameter => "%rand_([a-zA-Z0-9]+)\((.*)\)%",
+            Self::methodParameterWithId => "%rand_([a-zA-Z0-9]+)\(([a-zA-Z0-9,]+)\)_(\d+)%"
+        };
+    }
+
+    public function key(): array {
+
+        return match($this) {
+            Self::methodWithId => array('search', 'method', 'id'),
+            Self::method => array('search', 'method'),
+            Self::methodParameter => array('search', 'method', 'param'),
+            Self::methodParameterWithId => array('search', 'method', 'param','id')
+        };
+    }
+}
