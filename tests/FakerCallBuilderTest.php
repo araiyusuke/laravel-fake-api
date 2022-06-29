1f44642fa55bd7aa5532919e364ad455d9b00795
araiyusuke <info.yusuke.arai@gmail.com>
2022-06-29 16:26:24 +0900

    [削除] 不要なuse

--- tests/FakerCallBuilderTest.php
+++ tests/FakerCallBuilderTest.php
@@ -5,7 +5,6 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Araiyusuke\FakeApi\Faker\FakerCallBuilder;
 use Araiyusuke\FakeApi\Faker\DefaultFakerMethod;
-use Araiyusuke\FakeApi\Config\Collections\Method;
 use Mockery;
 
 class FakerCallBuilderTest extends TestCase
